c52ce94d7510f1f09f0de999039824047547d7bf
Claudio <soarescjunior@gmail.com>
2022-07-24 16:05:29 -0300

    Classe Runner do projeto

---
+++ src/test/java/buscarItens/Runner.java
@@ -0,0 +1,10 @@
+package buscarItens;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "classpath:features", publish = true, tags = "@Test")
+public class Runner {
+}
